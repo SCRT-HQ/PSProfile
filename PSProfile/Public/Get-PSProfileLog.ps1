70e8eebabb11a94b912452bf779d4ea08fd120ec
Nate Ferrell <nate@scrthq.com>
2019-07-28 13:34:13 -0500

    Updated PSProfile Logging

--- PSProfile/Public/Get-PSProfileLog.ps1
+++ PSProfile/Public/Get-PSProfileLog.ps1
@@ -16,15 +16,15 @@ function Get-PSProfileLog {
     )
     if ($Summary) {
         $Global:PSProfile.Log | Group-Object Section | ForEach-Object {
-            $section = $_[0].Name
+            $sectName = $_.Name
             $Group = $_.Group
             $sectCaps = $Group | Where-Object {$_.Message -match '^SECTION (START|END)$'}
             [PSCustomObject]@{
-                Name = $section
+                Name = $sectName
                 Start = $sectCaps[0].Time.ToString('HH:mm:ss.fff')
                 Section = "$([Math]::Round(($sectCaps[-1].Time - $sectCaps[0].Time).TotalMilliseconds))ms"
                 Full = "$([Math]::Round(($Group[-1].Time - $Group[0].Time).TotalMilliseconds))ms"
-                RunningJobs = Get-RSJob -State Running | Where-Object {$_.Name -match $Section} | Select-Object -ExpandProperty Name
+                RunningJobs = Get-RSJob -State Running | Where-Object {$_.Name -match $sectName} | Select-Object -ExpandProperty Name
             }
         } | Sort-Object Start | Format-Table -AutoSize
     }
