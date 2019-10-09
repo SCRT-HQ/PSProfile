d8f8fd1e94e151e1f11cb9d4a9dba273bebd7f59
Nate Ferrell <nate@scrthq.com>
2019-10-09 01:46:33 -0500

    updated Dispose() call on filewatcher

--- PSProfile/PSProfile.psm1
+++ PSProfile/PSProfile.psm1
@@ -4,19 +4,20 @@ if ([Environment]::UserInteractive -or ($null -eq [Environment]::UserInteractive
     $global:PSProfile = [PSProfile]::new()
     $global:PSProfile.Load()
     Export-ModuleMember -Variable PSProfile
-    $fsw = [System.IO.FileSystemWatcher]::new($(Split-Path $global:PSProfile.Settings.ConfigurationPath -Parent),'Configuration.psd1')
-    $global:PSProfileConfigurationWatcher = Register-ObjectEvent -InputObject $fsw -EventName Changed -Action {
+    $global:PSProfileConfigurationWatcher = [System.IO.FileSystemWatcher]::new($(Split-Path $global:PSProfile.Settings.ConfigurationPath -Parent),'Configuration.psd1')
+    $job = Register-ObjectEvent -InputObject $global:PSProfileConfigurationWatcher -EventName Changed -Action {
         [PSProfile]$conf = Import-Configuration -Name PSProfile -CompanyName 'SCRT HQ' -Verbose:$false
         $conf._internal = $global:PSProfile._internal
         $global:PSProfile = $conf
     }
     $PSProfile_OnRemoveScript = {
         try {
-            Remove-Variable PSProfileConfigurationWatcher -Scope Global -Force
-            Remove-Variable PSProfile -Scope Global -Force
             $global:PSProfileConfigurationWatcher.Dispose()
         }
-        catch {}
+        finally {
+            Remove-Variable PSProfile -Scope Global -Force
+            Remove-Variable PSProfileConfigurationWatcher -Scope Global -Force
+        }
     }
     $ExecutionContext.SessionState.Module.OnRemove += $PSProfile_OnRemoveScript
     Register-EngineEvent -SourceIdentifier ([System.Management.Automation.PsEngineEvent]::Exiting) -Action $PSProfile_OnRemoveScript
